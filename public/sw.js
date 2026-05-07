fcd9117caf1c6f4684dafdde40697affe72111d7
Cris <cr.96bc@gmail.com>
2026-05-07 11:18:47 +0300

    Convert app to PWA: add manifest, sw.js and icons

---
+++ public/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'braderm-hub-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/loghi/logo_braderm.png',
+  '/loghi/logo_lcb.png'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate Event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Event (Network First for most things)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
